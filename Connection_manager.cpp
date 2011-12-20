af1c89a931801724635248c3176c5addf126c2ae
Karel-Lodewijk Verdonck <karel.lodewijk@gmail.com>
2011-12-20 03:00:57 +0100

    Deleteted some files of the old version that were accidently left in place

--- Connection_manager.cpp
+++
@@ -1,7 +0,0 @@
-#include "Connection_manager.h"
-
-//implementation
-
-void Connection_manager::write(Tcp_connection* connection, const char* data, int size) {
-    connection->write(data, size);
-}
